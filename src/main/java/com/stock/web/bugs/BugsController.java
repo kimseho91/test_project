e2d62284e16c302e8450facdfa9eb6ffb39c2461
KimSeHo <91kimseho91@gmail.com>
2019-11-15 10:43:27 +0900

    update .gitignore

--- src/main/java/com/stock/web/bugs/BugsController.java
+++ src/main/java/com/stock/web/bugs/BugsController.java
@@ -1,5 +1,12 @@
 package com.stock.web.bugs;
 
-public class BugsController {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("bugss")
+public class BugsController {
+	private static final Logger logger = LoggerFactory.getLogger(BugsController.class);
 }
