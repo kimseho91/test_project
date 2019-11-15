9f59c365636410d9bc081de1c531c6a8c469129e
KimSeHo <91kimseho91@gmail.com>
2019-11-15 11:16:44 +0900

    2019-11-15

--- src/main/java/com/stock/web/pxy/CgvProxy.java
+++ src/main/java/com/stock/web/pxy/CgvProxy.java
@@ -1,5 +1,15 @@
 package com.stock.web.pxy;
 
-public class CgvProxy {
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component("cgvpxy")
+public class CgvProxy extends Proxy{
+	
+	@Transactional
+	public void insertCgv() {
+		for(int i=0 ; i<100 ; i++) {
+			
+		}
+	}
 }
--- src/main/java/com/stock/web/pxy/NaverProxy.java
+++ src/main/java/com/stock/web/pxy/NaverProxy.java
@@ -1,5 +1,15 @@
 package com.stock.web.pxy;
 
-public class NaverProxy {
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component("naverpxy")
+public class NaverProxy extends Proxy{
+	
+	@Transactional
+	public void insertCgv() {
+		for(int i=0 ; i<100 ; i++) {
+			
+		}
+	}
 }
